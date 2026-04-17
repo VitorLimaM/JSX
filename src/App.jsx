18723d8e14256900330ac0ecaebde7cb61493fd2
VitorLimaM <vitorfarialima@gmail.com>
2026-04-16 18:54:46 -0700

    Learn About Self-Closing JSX Tags

--- src/App.jsx
+++ src/App.jsx
@@ -41,3 +41,13 @@ const JSX5 = (
     <h1>Add a class to this div</h1>
   </div>
 );
+
+//6
+const JSX6 = (
+  <div>
+    <h2>Welcome to React!</h2>
+    <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+  </div>
+);
