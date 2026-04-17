af7a782d5719b9d6a9c7ce311e269e9921f7a7ae
VitorLimaM <vitorfarialima@gmail.com>
2026-04-16 19:01:59 -0700

    Create a Component with Composition

--- src/App.jsx
+++ src/App.jsx
@@ -77,3 +77,28 @@ class MyComponent1 extends React.Component {
   }
 };
 
+//9 
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        {/* Change code below this line */}
+        <ChildComponent />
+        {/* Change code above this line */}
+      </div>
+    );
+  }
+};
+
